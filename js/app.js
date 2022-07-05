94025b71e2ce14bf7d85fe87189d4c87a096ca69
kakhber <beridzekakha@gmail.com>
2022-07-05 17:58:12 +0400

    vvv

---
+++ js/app.js
@@ -0,0 +1,46 @@
+//1.
+function oddEven(a) {
+    if (a % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+} 
+//2.
+function quadrilateralArea(a,b) {
+    return a * b;
+}
+//3.
+function quadrilateralArea(a,b)
+    if (a = b) {
+        return a * b;
+    }
+//4.
+//5.
+function getCurrencySymbolFromCode (currency) {
+    if (currency === USD ) {
+        return $;
+    } else if (currency === EUR ) {
+        return €;
+    } else (currency === GEL)
+        return ლ;
+}
+//6.
+
+const peopleList = [
+{ name: `kakha`, age: 28,}
+{ name: `levani`, age: 31,}
+{ name: `irakli`, age: 38,}
+{ name: `madi`, age: 37,}
+{ name: `nikolozi`, age: 12,}
+]
+function theYoungest(peopleList) {
+    if (condition) {
+        return "Too young"
+        
+    }
+}
+//4.
+function getRandomNum(max) {
+    return Math.floor(Math.random() * max);
+}
